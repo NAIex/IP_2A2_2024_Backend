799023b66d10e4c47e10e5036b4f4d8a403f652a
helio18 <126113270+helio18@users.noreply.github.com>
2024-04-21 17:09:13 +0300

    update login

--- src/api/services/auth.services.js
+++ src/api/services/auth.services.js
@@ -5,53 +5,63 @@ import prisma from "../../prisma/index.js";
 
 class AuthService {
 
-    // just for testing, not the actual function
+    static async register(userData) {
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
     
-    // static async register(userData) {
-    //     const hashedPassword = await bcrypt.hash(userData.password, 10);
+        let newUser = await prisma.User.create({
+            data: {
+                email: userData.email,
+                password: hashedPassword
+            },
+        })
     
-    //     let newUser = await prisma.User.create({
-    //         data: {
-    //             email: userData.email,
-    //             password: hashedPassword
-    //         },
-    //     })
+        return newUser;
+    }
     
-    //     return newUser;
-    // }
-
     static async login(userData) {
         const { email, password } = userData;
 
-        const isAdmin = email.endsWith("@admin.uaic.ro");
+        let model;
+        if (email.endsWith("@admin.uaic.ro")) {
+            model = prisma.Admin;
+        } else if (email.endsWith("@profesor.uaic.ro") || email.endsWith("@student.uaic.ro")) {
+            model = prisma.User;
+        } else {
+            throw createError.NotFound('Email format not recognized');
+        }
 
-        const model = isAdmin ? prisma.Admin : prisma.User;
-    
         const user = await model.findUnique({
             where: { email: email },
         });
-    
+
         if (!user) throw createError.NotFound('User not registered');
 
         const checkPassword = await bcrypt.compare(password, user.password);
         if (!checkPassword) throw createError.Unauthorized('Email address or password not valid');
-    
+
         await model.update({
             where: { email: email },
             data: { log_status: true },
         });
-    
+
         const { password: _, ...userWithoutPassword } = user;
-    
+
         return userWithoutPassword;
     }
+
+    // just for testing, not the actual function
 
     static async logout(userData) {
         const { email } = userData;
 
-        const isAdmin = email.endsWith("@admin.uaic.ro");
-
-        const model = isAdmin ? prisma.Admin : prisma.User;
+        let model;
+        if (email.endsWith("@admin.uaic.ro")) {
+            model = prisma.Admin;
+        } else if (email.endsWith("@profesor.uaic.ro") || email.endsWith("@student.uaic.ro")) {
+            model = prisma.User;
+        } else {
+            throw createError.NotFound('Email format not recognized');
+        }
     
         const user = await model.findUnique({
             where: { email: email },
@@ -68,8 +78,7 @@ class AuthService {
     
         return { message: "Successfully logged out" };
     }
-    
-    
+        
     static async all() {
         const allUsers = await prisma.users.findMany();
         return allUsers;
