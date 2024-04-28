93ec14e6c5ea0399e944a27dc280ef10d6eb8c54
AdelinCojocaruA2 <cojocaruadelin@gmail.com>
2024-04-28 18:59:42 +0300

    finished banned users route

--- src/api/services/ban.services.js
+++ src/api/services/ban.services.js
@@ -14,12 +14,13 @@ class BanService {
         return allUsers;
     }
 
-    //Functia viewBanCandidates afiseaza exact ca viewBannedUsers momentan
-
     static async viewBanCandidates() {
         const allUsers = await prisma.User.findMany({
             where: {
-                ban_status: true,
+                warnings_count: { gt: 0 }
+            },
+            orderBy: {
+                warnings_count: 'desc'
             }
         });
         return allUsers;
@@ -40,7 +41,11 @@ class BanService {
 
         await model.update({
             where: { id: id },
-            data: { ban_status: false },
+            data: { 
+                ban_status: false,
+                warnings_count: 0,
+                unbanned_date: null 
+            },
         });
 
         return { message: "User unbanned succesfully" };
@@ -59,13 +64,22 @@ class BanService {
             where: { id: id },
         });
 
-        if (!user) throw createError.NotFound('User not registered');
+        if(!user) throw createError.NotFound('User not registered');
+
+        if(user.user_type == "admin") throw createError.BadRequest('Admins cannot be banned');
 
         if(user.ban_status == true) return { message: "User already banned" };
 
+        const localTimeOffset = new Date().getTimezoneOffset();
+        
+        const unbanDate = new Date(new Date().getTime() + (3 * 24 * 60 * 60 * 1000) - (localTimeOffset * 60 * 1000));
+
         await model.update({
             where: { id: id },
-            data: { ban_status: true },
+            data: { 
+                ban_status: true,
+                unbanned_date: unbanDate
+            },
         });
 
         return { message: "User banned succesfully. Message sent: " + await this.sendBanNotification(user.id)};
