f926b5e15ede9797527b8024f7a980d678a52fdc
Razvan_Boldeanu <rfboldeanu@gmail.com>
2024-05-14 22:20:00 +0300

    Update MutedUsers.js

--- src/api/routes/MutedUsers.js
+++ src/api/routes/MutedUsers.js
@@ -5,6 +5,16 @@ import mute from "../controllers/MutedUsersController.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /mute/viewMutedUsers:
+ *   get:
+ *     summary: View all muted users
+ *     description: Shows a list of all the muted users on the platform.
+ *     responses:
+ *       '200':
+ *         description: List of all the banned users
+ */
 router.get('/', mute.viewMutedUsers);
 router.get('/viewMutedUsers', mute.viewMutedUsers);
 router.post('/unmuteUser', mute.unmuteUser);
