3bad24469ef665f0eac87a42cfd832509680d7e2
sandu-motelica <motelica.sb@gmail.com>
2024-05-22 01:55:31 +0300

    add documentation

--- src/api/routes/Feed.js
+++ src/api/routes/Feed.js
@@ -5,6 +5,27 @@ import { getUserFeed } from "../controllers/FeedController.js";
 import auth from "../middlewares/auth.js";
 
 const router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: /feed
+ *   description: API for feed management
+ */
+
+/**
+ * @swagger
+ * :
+ *   get:
+ *     tags: [/feed]
+ *     summary: Get user feed
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User feed retrieved successfully
+ */
+
 router.get("/", auth, getUserFeed);
 
 export default router;
