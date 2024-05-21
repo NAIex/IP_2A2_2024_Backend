a13a10282cdd6a998e0fd98341e2e9abb6d77532
sandu-motelica <motelica.sb@gmail.com>
2024-05-21 21:53:03 +0300

    add documentation

--- src/api/routes/Thread.js
+++ src/api/routes/Thread.js
@@ -11,9 +11,77 @@ import auth from "../middlewares/auth.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: /thread
+ *   description: API for thread management
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     tags: [/thread]
+ *     summary: Return all threads
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of threads
+ */
 router.get("/", auth, getThreads);
+
+/**
+ * @swagger
+ * /community:
+ *   get:
+ *     tags: [/thread]
+ *     summary: Return all threads and their communities
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of threads and communities
+ */
 router.get("/community", auth, getThreadCommunity);
 
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     tags: [/thread]
+ *     summary: Add a new thread
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - description
+ *               - communityId
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the thread
+ *               description:
+ *                 type: string
+ *                 description: A description of the thread
+ *               communityId:
+ *                 type: integer
+ *                 description: The ID of the community to which the thread belongs
+ *     responses:
+ *       201:
+ *         description: Successfully created thread
+ *       404:
+ *         description: User or Community does not exist
+ *       500:
+ *         description: Server error
+ */
 router.post(
   "/",
   body("name").notEmpty(),
@@ -22,6 +90,37 @@ router.post(
   ErrorMiddleware,
   addThread
 );
+
+/**
+ * @swagger
+ * /:
+ *   delete:
+ *     tags: [/thread]
+ *     summary: Remove a thread
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - removeThreadId
+ *             properties:
+ *               removeThreadId:
+ *                 type: integer
+ *                 description: The ID of the thread to be removed
+ *     responses:
+ *       204:
+ *         description: Successfully removed thread
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User or Thread does not exist
+ *       500:
+ *         description: Server error
+ */
 router.delete(
   "/",
   body("removeThreadId").notEmpty(),
