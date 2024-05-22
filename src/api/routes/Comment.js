e1c59ea41a73967a4bbab7761964c7eec395d057
sandu-motelica <motelica.sb@gmail.com>
2024-05-22 15:18:54 +0300

    add documentation

--- src/api/routes/Comment.js
+++ src/api/routes/Comment.js
@@ -21,7 +21,7 @@ const router = Router();
 
 /**
  * @swagger
- * /:
+ * :
  *   get:
  *     tags: [/comment]
  *     summary: Return all comments
@@ -32,10 +32,68 @@ const router = Router();
  *         description: A list of comments
  */
 router.get("/", auth, getComments);
-
+/**
+ * @swagger
+ * /direct:
+ *   get:
+ *     tags: [/comment]
+ *     summary: Return all direct comments on threads
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of direct comments on threads
+ */
 router.get("/direct", auth, getThreadDirectComments);
-router.get("/subcomments", auth, getCommentSubcomment);
 
+/**
+ * @swagger
+ * /subcomment:
+ *   get:
+ *     tags: [/comment]
+ *     summary: Return all subcomments
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of subcomments
+ */
+router.get("/subcomment", auth, getCommentSubcomment);
+
+/**
+ * @swagger
+ * :
+ *   post:
+ *     tags: [/comment]
+ *     summary: Add a new direct comment
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - content
+ *               - threadId
+ *             properties:
+ *               content:
+ *                 type: string
+ *                 description: The content of the comment
+ *               threadId:
+ *                 type: integer
+ *                 description: The ID of the thread to comment on
+ *     responses:
+ *       201:
+ *         description: Successfully added comment
+ *       404:
+ *         description: User or Thread does not exist
+ *       401:
+ *         description: Permission denied
+ *       500:
+ *         description: Server error
+ */
 router.post(
   "/",
   body("content").notEmpty(),
@@ -44,6 +102,45 @@ router.post(
   ErrorMiddleware,
   addDirectComment
 );
+
+/**
+ * @swagger
+ * /subcomment:
+ *   post:
+ *     tags: [/comment]
+ *     summary: Add a new subcomment
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - content
+ *               - commentId
+ *               - threadId
+ *             properties:
+ *               content:
+ *                 type: string
+ *                 description: The content of the subcomment
+ *               commentId:
+ *                 type: integer
+ *                 description: The ID of the comment to respond to
+ *               threadId:
+ *                 type: integer
+ *                 description: The ID of the thread the comment belongs to
+ *     responses:
+ *       201:
+ *         description: Successfully added subcomment
+ *       404:
+ *         description: User, Comment, or Thread does not exist
+ *       401:
+ *         description: Permission denied
+ *       500:
+ *         description: Server error
+ */
 router.post(
   "/subcomment",
   body("content").notEmpty(),
