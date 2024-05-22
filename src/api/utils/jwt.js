548446fb2137dfd51c928e8f5f29d0de10f3aea4
sandu-motelica <motelica.sb@gmail.com>
2024-05-22 23:02:02 +0300

    sync

--- src/api/utils/jwt.js
+++ src/api/utils/jwt.js
@@ -54,7 +54,7 @@ export async function signAccessToken(userId, email, isAdmin) {
     const payload = { userId, email, isAdmin };
     const token = jwt.sign(payload, privateKey, {
       algorithm: "RS256",
-      expiresIn: "1m",
+      expiresIn: "200m",
     });
     return token;
   } catch (error) {
