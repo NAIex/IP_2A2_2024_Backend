35cef8f5567cd364aee790617c6041cc4081f2dc
sandu-motelica <motelica.sb@gmail.com>
2024-05-24 17:30:49 +0300

    add additional information to feed

--- src/api/controllers/FeedController.js
+++ src/api/controllers/FeedController.js
@@ -1,9 +1,15 @@
 import prisma from "../../prisma/index.js";
-
 export const getUserFeed = async (req, res) => {
   const userId = req.user.userId;
-
+  const userEmail = req.user.email;
   try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId, email: userEmail },
+    });
+    if (!user) {
+      res.status(404).send("User does not exist");
+      return;
+    }
     const userCommunities = await prisma.communityUser.findMany({
       where: { user_id: userId },
       select: { community_id: true },
@@ -11,22 +17,29 @@ export const getUserFeed = async (req, res) => {
 
     const communityIds = userCommunities.map((c) => c.community_id);
 
-    const threads = await prisma.communityThread.findMany({
+    const communityThreads = await prisma.communityThread.findMany({
       where: {
         community_id: { in: communityIds },
       },
     });
-    const threadsIds = threads.map((thread) => thread.thread_id);
 
-    const recentThreads = await prisma.thread.findMany({
-      where: {
-        id: { in: threadsIds },
-        // is_archived: false,
-      },
-      orderBy: { creation_time: "desc" },
-    });
+    const threadsWithCommunityTitle = await Promise.all(
+      communityThreads.map(async (communityThread) => {
+        const thread = await prisma.thread.findUnique({
+          where: { id: communityThread.thread_id },
+        });
+        const community = await prisma.community.findUnique({
+          where: { id: communityThread.community_id },
+          select: { name: true }, // Select only the community title
+        });
+        return {
+          ...thread,
+          communityTitle: community.name,
+        };
+      })
+    );
 
-    res.status(200).json(recentThreads);
+    res.status(200).json(threadsWithCommunityTitle);
   } catch (e) {
     res.status(500).json(e);
   }
